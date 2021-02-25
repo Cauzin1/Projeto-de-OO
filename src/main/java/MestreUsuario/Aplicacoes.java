346896bb33773ced1a256a193804ba49fc922c7c
Cauzin1 <thomas.causin@estudante.ufjf.br>
2021-02-25 18:36:30 -0300

    Inicio do projeto Time 7. Basicamente começamos a construir a interface do menu e criamos algumas classes que serão usadas futuramente.

---
+++ src/main/java/MestreUsuario/Aplicacoes.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MestreUsuario;
+import java.util.Date; 
+/**
+ *
+ * @author Causin
+ */
+public class Aplicacoes {
+    float valor;
+    int estoque;
+    int contador;
+    
+    public double valtotal(){
+        //somatorio das compras
+        //fazer um if com o estoque para ver se é possivel comprar
+        return 1;
+    }
+    public int armazena(){
+        //usar um contador para ver quantas vezes o usuario entrou
+        //na pagina com o intuito de saber o gosto dele
+        //ideia: usar um contador como base
+        return 1;
+    }
+    public double custoCamisa(){
+        return 1;
+        //utilizar a variável capital e aqui tentar fazer um esboço 
+        //do quanto custa para produzir uma camisa;
+        //obs: fazer uma interface para escrever os dados e mostrar o retorno 
+        //com a funcao;
+    }
+    
+}
+
---
+++ src/main/java/MestreUsuario/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MestreUsuario;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Causin
+ */
+public class Usuario {
+    String loginU;
+    String emailU;
+    String senhaU;
+    Date aniversario;
+    
+}
